64ca235a14c746da79489842ed4fe031e33464ca
Jose Iriarte <iriarte.joselito@gmail.com>
2018-04-25 23:35:37 -0400

    add Day 1 Lab | First SpringBoot Application

---
+++ src/main/java/com/ji/project/jiriartespringboot/JiriarteSpringbootApplication.java
@@ -0,0 +1,32 @@
+package com.ji.project.jiriartespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+
+public class JiriarteSpringbootApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	@RequestMapping("/hello")
+	String hello(){
+		return "Hello!!";
+	}
+
+	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+	@ResponseBody
+	public String getNameBySimplePathWithPathVariable(
+			@PathVariable("name") String name) {
+		return "Hello " + name + "!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiriarteSpringbootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ji/project/jiriartespringboot/JiriarteSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ji.project.jiriartespringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JiriarteSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
