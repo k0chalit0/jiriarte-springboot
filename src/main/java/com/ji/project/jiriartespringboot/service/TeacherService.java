90c9be9f4d0ccf4de296408d43cbf3f6fd9e958a
Jose Iriarte <iriarte.joselito@gmail.com>
2018-04-27 21:46:57 -0400

    Lab Day 3 - add endpoints, add config swagger

--- src/main/java/com/ji/project/jiriartespringboot/service/TeacherService.java
+++ src/main/java/com/ji/project/jiriartespringboot/service/TeacherService.java
@@ -1,6 +1,5 @@
 package com.ji.project.jiriartespringboot.service;
 
-
     import com.ji.project.jiriartespringboot.domain.Teacher;
     import org.springframework.stereotype.Service;
     import java.util.ArrayList;
@@ -10,12 +9,50 @@ package com.ji.project.jiriartespringboot.service;
     @Service
     public class TeacherService {
 
+        private static final List<Teacher> listTeacher = new ArrayList<>();
+
+        public TeacherService(){
+            listTeacher.add(new Teacher(0, "Jose"));
+            listTeacher.add(new Teacher(1, "Jhonny"));
+            listTeacher.add(new Teacher(2, "Gabriel"));
+            listTeacher.add(new Teacher(3, "Daniela"));
+        }
+        // CRUD
+
+        /* List Teachers */
         public List<Teacher> getAllTeachers(){
+            return listTeacher;
+        }
+
+        /* Add Teacher */
+        public void addNewTeacher( Teacher newTeacher){
+            // insert new Teacher into DB
+            listTeacher.add(newTeacher);
+        }
+
+        /* Delete Teacher by Id*/
+        public void deleteTeacher(int id){
+            listTeacher.remove(id);
+        }
+
+        /* Get a Teacher by Id*/
+        public Teacher getTeacherById(int id){
+            //get a Teacher by id from DB
+            return listTeacher.get(id);
+        }
+
+        /* Delete Teacher by Object */
+        /*
+        public void deleteTeacherByTeacher(Teacher teacher){
+            listTeacher.remove(teacher);
+        }*/
+
+        /*public List<Teacher> getAllTeachers(){
             List<Teacher> listTeachers = new ArrayList<>();
             listTeachers.add(new Teacher(1, "jhonny"));
             listTeachers.add(new Teacher(2, "Gabriel"));
             listTeachers.add(new Teacher(3, "Su"));
             return listTeachers;
-        }
+        }*/
 
     }
--- src/main/java/com/ji/project/jiriartespringboot/web/TeacherController.java
+++ src/main/java/com/ji/project/jiriartespringboot/web/TeacherController.java
@@ -3,21 +3,49 @@ package com.ji.project.jiriartespringboot.web;
 
 import com.ji.project.jiriartespringboot.domain.Teacher;
 import com.ji.project.jiriartespringboot.service.TeacherService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 @RequestMapping("/teachers")
+@Api(value="onlinestore", description="Operations pertaining a Teachers Service")
 public class TeacherController {
 
     @Autowired
-    TeacherService teacherService;
+    private TeacherService teacherService;
+
     @RequestMapping(method = RequestMethod.GET)
-    public List<Teacher> getTeacher(){
+    public List<Teacher> getTeachers(){
         return teacherService.getAllTeachers();
+    }
+
+    //add Teacher
+    @RequestMapping(method = RequestMethod.POST)
+    public void addNewTeacher(@RequestBody Teacher newTeacher){
+        System.out.println("the Name of new Teacher is "+newTeacher.getName());
+        teacherService.addNewTeacher(newTeacher);
+    }
+
+    //get Teacher
+    @RequestMapping(path="/{id}", method = RequestMethod.GET)
+    public Teacher getTeacherById(@PathVariable int id){
+        return teacherService.getTeacherById(id);
+    }
+
+    //delete Teacher
+    @RequestMapping(path="/{id}", method = RequestMethod.DELETE)
+    public void deleteTeacher(@PathVariable int id){
+        System.out.println("the Id delete is "+ id);
+        teacherService.deleteTeacher(id);
+    }
+
+    //delete Teacher by Object
+    @RequestMapping( method = RequestMethod.DELETE)
+    public void deleteTeacherByObject(@RequestBody Teacher delTeacher){
+        System.out.println("the Teacher delete is "+ delTeacher.getName());
+        teacherService.deleteTeacher( (int) delTeacher.getId() );
     }
 }
 
